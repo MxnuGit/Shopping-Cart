036989827c1f4e2d5250bc287fb8b6bf2c638b0c
Menghetz <manuelmenghetti0@gmail.com>
2026-03-12 08:36:29 +0100

    init with first initialization

---
+++ app/src/main/java/com/example/shoppingcart/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.shoppingcart
+
+import android.os.Bundle
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val editTxt = findViewById<EditText>(R.id.editTxt);
+        val userInput = editTxt.text;
+    }
+}
