def583cdede7d11a9853dc3f783ba45311f34059
Menghetz <manuelmenghetti0@gmail.com>
2026-03-13 20:23:58 +0100

    Add: first 'binding' between edit text and button

--- app/src/main/java/com/example/shoppingcart/MainActivity.kt
+++ app/src/main/java/com/example/shoppingcart/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.shoppingcart
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +14,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val editTxt = findViewById<EditText>(R.id.editTxt);
-        val userInput = editTxt.text;
+        val userBtn =  findViewById<Button>(R.id.addBtn);
+        val itemList = ArrayList<String>()
+
+        userBtn.setOnClickListener {
+            val text = editTxt.text.toString();
+            itemList.add(text)
+            Log.d("item", "$itemList")
+            editTxt.setText("")
+        }
     }
 }
